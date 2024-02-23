4a1e40a8d6b403584d72b2283dcc0cb598f7a61e
Lilia <lilia@student.dev-island.org>
2024-02-22 18:45:31 -1000

    setup overall layout and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>React Contacts</h1>
+
+      <div className="contact-card">
+        <h3>Jenny Smith</h3>
+        <p>Phone: 808-867-5309</p>
+        <p>Email: jenny@gmail.com</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
