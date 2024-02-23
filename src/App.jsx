c6d4b35bb3a2a5a6d03b577f3dcc2d80f7e3e050
Lilia <lilia@student.dev-island.org>
2024-02-22 19:00:22 -1000

    create contact card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import ContactCard from "./components/ContactCard/ContactCard";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>React Contacts</h1>
-
-      <div className="contact-card">
-        <h3>Jenny Smith</h3>
-        <p>Phone: 808-867-5309</p>
-        <p>Email: jenny@gmail.com</p>
-      </div>
+     <h1>React Contacts</h1>
+      <ContactCard 
+        name="Jenny Smith"
+        phone="808-867-5309"
+        e-mail="jenny@gmail.com"
+        />
     </>
   );
 }
