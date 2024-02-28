1c781e1b31923700a0bfcc3d80d9d7c088ad1698
Lilia <lilia@student.dev-island.org>
2024-02-27 20:45:48 -1000

    Implement ContactForm component

--- src/App.jsx
+++ src/App.jsx
@@ -2,103 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList/ContactList";
 import dummyContacts from "./contacts.js";
-import MaskedInput from "react-text-mask";
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
+
 
 function App() {
   const [contacts, setContacts] = useState(dummyContacts);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState(" ");
-  const [email, setEmail] = useState(" ");
 
   return (
     <>
       <h1>React Contacts</h1>
 
-      <form className="form-container">
-        <label>
-          First Name:{" "}
-          <input
-            type="text"
-            className="form-input"
-            onChange={(event) => {
-              const value = event.target.value;
-              setFirstName(value);
-            }}
-          />
-        </label>
-
-        <label>
-          Last Name:{" "}
-          <input
-            type="text"
-            className="form-input"
-            onChange={(event) => {
-              const value = event.target.value;
-              setLastName(value);
-              console.log(lastName);
-            }}
-          />
-        </label>
-
-        <label>
-          Phone Number:{" "}
-          <MaskedInput
-            mask={[
-              /\d/,
-              /\d/,
-              /\d/,
-              "-",
-              /\d/,
-              /\d/,
-              /\d/,
-              "-",
-              /\d/,
-              /\d/,
-              /\d/,
-              /\d/,
-            ]}
-            guide={true}
-            className="form-input"
-            onChange={(event) => {
-              const value = event.target.value;
-              setPhoneNumber(value);
-            }}
-          />
-        </label>
-
-        <label>
-          Email Address:{" "}
-          <input
-            type="email"
-            className="form-input"
-            pattern=".+@example\.com"
-            onChange={(event) => {
-              const value = event.target.value;
-              setEmail(value);
-            }}
-          ></input>
-        </label>
-
-        <button
-          type="submit"
-          className="form-buttom"
-          onClick={(event) => {
-            event.preventDefault();
-            const newContact = {
-              firstName,
-              lastName,
-              phoneNumber,
-              email,
-            };
-            const updatedContacts = [...contacts, newContact];
-            setContacts(updatedContacts);
-            console.log("Submit clicked!");
-          }}
-        >
-          Create Contact
-        </button>
-      </form>
+      <ContactForm
+        onFormSubmissionHandler={(newContact) => {
+          const updatedContacts = [...contacts, newContact];
+          setContacts(updatedContacts);
+        }}
+      />
 
       <ContactList contacts={contacts} />
     </>
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,91 @@
+import "./ContactForm.css";
+import { useState } from "react";
+import MaskedInput from "react-text-mask";
+
+export default function ContactForm(onFormSubmissionHandler) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(" ");
+  const [email, setEmail] = useState(" ");
+
+  return (
+    <form className="form-container">
+      <label>
+        First Name:{" "}
+        <input
+          type="text"
+          className="form-input"
+          onChange={(event) => {
+            const value = event.target.value;
+            setFirstName(value);
+          }}
+        />
+      </label>
+
+      <label>
+        Last Name:{" "}
+        <input
+          type="text"
+          className="form-input"
+          onChange={(event) => {
+            const value = event.target.value;
+            setLastName(value);
+            console.log(lastName);
+          }}
+        />
+      </label>
+
+      <label>
+        Phone Number:{" "}
+        <MaskedInput
+          mask={[
+            /\d/,
+            /\d/,
+            /\d/,
+            "-",
+            /\d/,
+            /\d/,
+            /\d/,
+            "-",
+            /\d/,
+            /\d/,
+            /\d/,
+            /\d/,
+          ]}
+          guide={true}
+          className="form-input"
+          onChange={(event) => {
+            const value = event.target.value;
+            setPhoneNumber(value);
+          }}
+        />
+      </label>
+
+      <label>
+        Email Address:{" "}
+        <input
+          type="email"
+          className="form-input"
+          pattern=".+@example\.com"
+          onChange={(event) => {
+            const value = event.target.value;
+            setEmail(value);
+          }}
+        ></input>
+      </label>
+
+      <button
+        type="submit"
+        className="form-buttom"
+        onClick={(event) => {
+          event.preventDefault();
+
+          const newContact = { firstName, lastName, phoneNumber, email };
+          onFormSubmissionHandler(newContact);
+        }}
+      >
+        Create Contact
+      </button>
+    </form>
+  );
+}
