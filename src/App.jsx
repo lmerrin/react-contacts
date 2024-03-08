691e3ce65443ec9b863b2a0b1da27a3a847078e4
Lilia <lilia@student.dev-island.org>
2024-03-07 18:55:25 -1000

    Implement basic search functionaliy on name

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ContactForm from "./components/ContactForm/ContactForm.jsx";
 
 function App() {
   const [contacts, setContacts] = useState(dummyContacts);
+  const [searchText, setSearchText] = useState("");
 
   return (
     <>
@@ -24,13 +25,34 @@ function App() {
           setContacts(updatedContacts);
         }}
       />
+      <div>
+        <input
+          onChange={(event) => {
+            setSearchText(event.target.value);
+          }}
+          type="text"
+          placeholder="Search Contacts"
+        />
+        <button
+          onClick={() => {
+            const updatedContacts = contacts.filter((contact) => {
+              const lowerCaseName = `
+                ${contact.firstName} ${contact.lastName}`.toLowerCase();
+              return lowerCaseName.includes(searchText.toLowerCase());
+            });
+            setContacts(updatedContacts);
+          }}
+        >
+          Search
+        </button>
+      </div>
 
       <ContactList
         contacts={contacts}
         onCardDeleteClicked={(phoneNumber) => {
           const updatedContacts = contacts.filter((contact) => {
             return contact.phoneNumber !== phoneNumber;
-          })
+          });
           setContacts(updatedContacts);
         }}
       />
